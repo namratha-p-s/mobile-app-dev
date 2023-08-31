edd65571f4f86fc3b9479b363b7c9f5bed1a3060
Namratha P S <namratha.psom.97@gmail.com>
2023-08-31 10:45:15 -0600

    Initial Commit

---
+++ mad/madApp.swift
@@ -0,0 +1,17 @@
+//
+//  madApp.swift
+//  mad
+//
+//  Created by Narmada on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct madApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
