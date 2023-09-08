d1bce64b654cef200881ddcc62e36e57abf852a1
Namratha P S <namratha.psom.97@gmail.com>
2023-09-07 19:21:31 -0600

    Added Rotation Control View

--- mad/ContentView.swift
+++ mad/ContentView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct ContentView: View {
     @State private var rotation: Double = 0.0
     @State private var isRotationEnabled: Bool = false
-    @State private var lastRotationAngle: Double = 0.0
     
     var body: some View {
         ZStack {
@@ -18,16 +17,14 @@ struct ContentView: View {
                 .ignoresSafeArea()
             
             VStack {
-                Toggle("Rotate Earth", isOn: $isRotationEnabled)
-                    .foregroundColor(.white)
-                    .padding()
-                    .onChange(of: isRotationEnabled) { newValue in
-                            lastRotationAngle = rotation
-                    }
+                // Rotate the Earth with the help of a slider
+                RotationControlView(rotation: $rotation, isRotationEnabled: $isRotationEnabled)
                 
+                // Additionally, rotate the earth by clicking it in 45 degrees
                 Image("world")
                     .resizable()
                     .cornerRadius(15)
+                    .clipShape(RoundedRectangle(cornerRadius: 100))
                     .aspectRatio(contentMode: .fit)
                     .padding()
                     .rotationEffect(.degrees(rotation))
@@ -46,7 +43,26 @@ struct ContentView: View {
                 
                 Text("(Tap to rotate the Earth)")
                     .foregroundColor(.white)
-                
+            }
+        }
+    }
+}
+
+struct RotationControlView: View {
+    @Binding var rotation: Double
+    @Binding var isRotationEnabled: Bool
+    
+    var body: some View {
+        VStack {
+            Toggle("Rotate Earth", isOn: $isRotationEnabled)
+                .foregroundColor(.white)
+                .padding()
+            
+            if isRotationEnabled {
+                Slider(value: $rotation, in: 0.0...360.0, step: 1.0) {
+                    Text("Rotation")
+                }
+                .padding()
             }
         }
     }
